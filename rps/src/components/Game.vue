40c20a613d6d2c84feb54e458dd7584be78b4d26
Ifeoluwa <iamsamnice@gmail.com>
2024-11-27 12:07:53 -0800

    Buiding Functionality

--- rps/src/components/Game.vue
+++ rps/src/components/Game.vue
@@ -1,26 +1,41 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Triangle from "./icons/IconTriangle.vue";
 import Scissors from "./icons/IconScissors.vue";
 import Rock from "./icons/IconRock.vue";
 import Paper from "./icons/IconPaper.vue";
 import Options from "@/assets/Options.json";
+
+// Define a reactive variable to store the selected index
+const selectedIndex = ref<number | null>(null);
+
+// Function to handle click and set the selected index
+const handleSelect = (index: number) => {
+  selectedIndex.value = index;
+  console.log("Selected index:", index); // Optional: For debugging
+};
 </script>
 
 <template>
   <div
-    class="flex justify-center items-center h-full w-full max-w-7xl border-2 border-red-500"
+    class="flex flex-col gap-8 md:flex-row justify-center items-center md:h-full w-full max-w-7xl max-h-[700px]"
   >
-    <div class="relative">
+    <div
+      class="relative w-full h-1/2 md:h-full flex justify-center items-center"
+    >
       <Triangle />
       <div
-        v-for="(option, index) in Options"
-        :key="index"
-        class="absolute top-0 grid grid-cols-2 gap-12"
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 grid grid-cols-2 gap-12 place-items-center w-60 md:w-72 lg:w-96 h-72 md:h-96"
       >
         <div
+          v-for="(option, index) in Options"
+          :key="index"
+          @click="handleSelect(index)"
           :class="[
-            'w-44 h-44 flex items-center justify-center -top-20  relative',
-            index === 1 ? '-right-5' : '',
+            'w-36 h-36 md:w-36 md:h-36 lg:w-44 lg:h-44 flex items-center justify-center absolute',
+            index === 0 ? '-top-10 md:top-0 -left-10' : '',
+            index === 1 ? '-top-10 md:top-0 -right-10' : '',
+            index === 2 ? 'bottom-0' : '',
           ]"
         >
           <div
@@ -39,38 +54,6 @@ import Options from "@/assets/Options.json";
         </div>
       </div>
     </div>
-    <!-- 
-       -->
-    <!-- <div
-          class="w-44 h-44 flex items-center justify-center -top-20 -right-5 relative"
-        >
-          <div
-            class="w-full h-full bg-[#ec9e0e] absolute top-2 rounded-full"
-          ></div>
-          <div class="w-full h-full bg-[#eca922] absolute rounded-full"></div>
-          <div
-            class="w-3/4 h-3/4 bg-slate-300 absolute top-3 mt-2 rounded-full"
-          ></div>
-          <div class="w-3/4 h-3/4 bg-zinc-200 absolute mt-2 rounded-full"></div>
-          <Scissors class="absolute" />
-        </div> -->
-    <!-- 
-       -->
-    <!-- <div
-          class="w-44 h-44 flex items-center justify-center relative col-span-2 justify-self-center bottom-24"
-        >
-          <div
-            class="w-full h-full bg-[#dc2e4e] absolute top-2 rounded-full"
-          ></div>
-          <div class="w-full h-full bg-[#dd405d] absolute rounded-full"></div>
-          <div
-            class="w-3/4 h-3/4 bg-slate-300 absolute top-3 mt-2 rounded-full"
-          ></div>
-          <div class="w-3/4 h-3/4 bg-zinc-200 absolute mt-2 rounded-full"></div>
-          <Scissors class="absolute" />
-        </div>
-      </div>
-    </div> -->
 
     <!-- <div
       class="w-2/3 h-full border-2 border-green-500 flex items-center justify-between"
@@ -93,7 +76,7 @@ import Options from "@/assets/Options.json";
           <Scissors class="absolute" />
         </div>
       </div>
-      <div class="text-zinc-100 text-4xl font-bold">
+      <div class="text-zinc-100 text-4xl font-bold mx-auto md:mx-0">
         <p>YOU WIN</p>
         <button class="bg-white text-red-600 text-sm w-40 h-10 rounded-md">
           PLAY AGAIN
--- rps/src/views/MainView.vue
+++ rps/src/views/MainView.vue
@@ -1,22 +1,54 @@
 <script setup lang="ts">
-import ScoreBoard from "@/components/ScoreBoard.vue";
+import { ref } from "vue";
+import Logo from "@/components/icons/IconLogo.vue";
 import Game from "@/components/Game.vue";
+import Rules from "@/components/icons/IconRules.vue";
+import Close from "@/components/icons/IconClose.vue";
+
+const rules = ref<boolean>(false);
 </script>
 
 <template>
-  <main class="w-full h-screen bg-[#141539] flex flex-col py-3">
+  <main
+    class="w-full min-h-screen max-h-[700px] bg-[#141539] flex flex-col py-3 relative"
+  >
     <header>
-      <ScoreBoard />
+      <div
+        class="w-11/12 md:w-2/3 md:h-36 border-zinc-700 border-2 rounded-lg mx-auto flex justify-between items-center px-5 py-2 max-w-7xl"
+      >
+        <div class="text-xl md:text-3xl text-zinc-100 font-bold">
+          <Logo />
+        </div>
+        <div
+          class="w-24 h-20 md:w-36 md:h-32 bg-zinc-100 rounded-lg flex flex-col justify-center items-center"
+        >
+          <p>Score</p>
+          <p class="text-4xl md:text-7xl">12</p>
+        </div>
+      </div>
     </header>
-    <div class="flex justify-center items-center flex-grow pt-14">
+    <div class="flex justify-center items-center flex-grow pt-10">
       <Game />
     </div>
     <footer class="self-end">
       <button
-        class="border-2 w-24 flex justify-center items-center h-10 rounded-md mr-5 text-zinc-100 font-bold"
+        @click="rules = true"
+        class="border-2 w-24 flex justify-center items-center h-10 rounded-md mr-5 text-zinc-100 font-bold mt-3 md:mt-0"
       >
         RULES
       </button>
     </footer>
+    <div
+      v-show="rules"
+      class="w-11/12 h-5/6 sm:w-96 sm:h-96 border bg-zinc-200 rounded-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center"
+    >
+      <div class="relative w-full h-full flex justify-center items-center">
+        <Close
+          @click="rules = !rules"
+          class="absolute right-1 top-1 cursor-pointer"
+        />
+        <Rules />
+      </div>
+    </div>
   </main>
 </template>
